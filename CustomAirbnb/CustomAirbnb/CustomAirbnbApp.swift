a699aabce485d85e7dc4bb2043dd6af421ca7c6f
Oriol Angelet <uriaeri@gmail.com>
2023-12-11 18:25:46 +0100

    Initial commit - Project creation

---
+++ CustomAirbnb/CustomAirbnb/CustomAirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAirbnbApp.swift
+//  CustomAirbnb
+//
+//  Created by Uri on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
