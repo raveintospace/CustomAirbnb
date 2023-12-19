51de1a73f7833a07fc1632cbbd21692b961f59e2
Oriol Angelet <uriaeri@gmail.com>
2023-12-19 19:07:25 +0100

    First iteration to download images for ListingRowView

---
+++ CustomAirbnb/CustomAirbnb/Core/Components/ListingImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ListingImageView.swift
+//  CustomAirbnb
+//
+//  Created by Uri on 19/12/23.
+//
+
+import SwiftUI
+
+struct ListingImageView: View {
+    
+    let listing: Listing
+    
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct ListingImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingImageView(listing: dev.listing)
+    }
+}
---
+++ CustomAirbnb/CustomAirbnb/Services/ListingImageDataService.swift
@@ -0,0 +1,55 @@
+//
+//  ListingImageDataService.swift
+//  CustomAirbnb
+//
+//  Created by Uri on 19/12/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    // exclusive cancellable for getCoinImage
+    private var imageSubscription: AnyCancellable?
+    
+    private let listing: Listing
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "listing_images"  // -> custom name for the folder where we store coin images
+    private let imageName: String
+    
+    init(listing: Listing) {
+        self.listing = listing
+        self.imageName = listing.id
+        getListingImage()
+    }
+    
+    private func getListingImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadListingImage()
+        }
+    }
+    
+    private func downloadListingImage() {
+        
+        guard let url = URL(string: listing.xlPictureURL ?? "") else { return }
+        
+        imageSubscription = NetworkManager.fetch(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: {
+                [weak self] (returnedImage) in
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel() // -> cancel because it won't call api continously
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
+            })
+    }
+}
