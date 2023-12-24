1e72335e210c44bdb48479cb6378e45e11257647
Oriol Angelet <uriaeri@gmail.com>
2023-12-24 13:06:47 +0100

    HomeView sends correct listing to DetailView

--- CustomAirbnb/CustomAirbnb/Core/Home/Views/HomeView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Home/Views/HomeView.swift
@@ -25,7 +25,7 @@ struct HomeView: View {
                 }
             }
             .navigationDestination(isPresented: $showDetailView) {
-                    Rectangle()
+                    DetailLoadingView(listing: $selectedListing)
                 }
         }
     }
