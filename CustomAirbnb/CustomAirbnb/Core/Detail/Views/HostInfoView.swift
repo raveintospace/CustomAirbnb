d9bdc9995b260742b6f6f48a56d095a15ce7e4f1
Oriol Angelet <uriaeri@gmail.com>
2024-01-05 18:39:10 +0100

    starsRating first iteration

--- CustomAirbnb/CustomAirbnb/Core/Detail/Views/HostInfoView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Detail/Views/HostInfoView.swift
@@ -24,6 +24,7 @@ struct HostInfoView: View {
 struct HostInfoView_Previews: PreviewProvider {
     static var previews: some View {
         HostInfoView(listing: dev.listing)
+            .padding(.horizontal)
     }
 }
 
@@ -33,7 +34,7 @@ extension HostInfoView {
         VStack(alignment: .leading) {
             Text(listing.hostName ?? "Name n/a")
                 .bold()
-            Image(systemName: "star.fill")
+            starsStack
             
             if listing.hostListingsCount == 1 {
                 Text("\(listing.hostListingsCount) listing")
@@ -41,6 +42,29 @@ extension HostInfoView {
                 Text("\(listing.hostListingsCount) listings")
             }
         }
+    }
+    
+    private var starsStack: some View {
+        let stars = HStack(spacing: 0) {
+            ForEach(0..<5, id: \.self) { _ in
+                Image(systemName: "star.fill")
+                    //.resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        
+        return stars.overlay(
+            GeometryReader { g in
+                let width = 3 / CGFloat(5) * g.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.yellow)
+                }
+            }
+                .mask(stars)
+        )
+        .foregroundColor(.gray)
     }
     
     private var bookButton: some View {
