94eb8871b22da7c7e8f66d494101e9f44693baa8
Oriol Angelet <uriaeri@gmail.com>
2024-01-09 18:48:26 +0100

    Favorites management logic set in viewModel: subscriber added, map extracted as private func & func to updateFavorites that View will call

--- CustomAirbnb/CustomAirbnb/Core/Home/ViewModel/HomeViewModel.swift
+++ CustomAirbnb/CustomAirbnb/Core/Home/ViewModel/HomeViewModel.swift
@@ -25,6 +25,8 @@ final class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
+        
+        // subscription to listingDataService & searchText
         $searchText
             .combineLatest(listingDataService.$allListings)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
@@ -34,6 +36,21 @@ final class HomeViewModel: ObservableObject {
                 self.allListings = returnedListings
             }
             .store(in: &cancellables)
+        
+        // subscription to allListings (already filtered with searchText) & favoritesDataService
+        $allListings
+            .combineLatest(favoritesDataService.$savedEntities)
+            .map(mapAllListingsToFavoriteListings)
+            .sink { [weak self] (returnedListings) in
+                guard let self = self else { return }
+                self.favoriteListings = returnedListings
+            }
+            .store(in: &cancellables)
+        
+    }
+    
+    func updateFavorites(listing: Listing) {
+        favoritesDataService.updatePortfolio(listing: listing)
     }
     
     // extracted .map from $searchText
@@ -48,6 +65,19 @@ final class HomeViewModel: ObservableObject {
             return listing.nameToSearch.lowercased().contains(lowercasedText) ||
             listing.hoodToSearch.lowercased().contains(lowercasedText)
         }
+    }
+    
+    // extracted .map from $allListings
+    private func mapAllListingsToFavoriteListings(allListings: [Listing], favoriteEntities: [FavoriteEntity]) -> [Listing] {
+        
+        // return nil for listings not included in favs
+        allListings
+            .compactMap { (listing) -> Listing? in
+                guard let entity = favoriteEntities.first(where: { $0.listingID == listing.id }) else {
+                    return nil
+                }
+                return listing
+            }
     }
 }
 
