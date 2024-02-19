d6b30e1e955fa82aece4568063bcf7230fdc9340
Oriol Angelet <uriaeri@gmail.com>
2024-02-19 19:14:04 +0100

    PhotosPickerView ui iteration

--- CustomAirbnb/CustomAirbnb/Core/Upload/Views/PhotosPickerView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Upload/Views/PhotosPickerView.swift
@@ -13,31 +13,34 @@ struct PhotosPickerView: View {
     @State private var imagePreviews: [UIImage] = []
     @State private var photosPickerItems: [PhotosPickerItem] = []
     
+    // properties for LazyHGrid
+    private let rows: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    private let spacing: CGFloat = 30
+    
     var body: some View {
         VStack {
             PhotosPicker(selection: $photosPickerItems, maxSelectionCount: 10, selectionBehavior: .ordered, matching: .images) {
-                    Image(systemName: "camera")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .foregroundStyle(Color.theme.accent)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.theme.secondaryText.opacity(0.5), lineWidth: 1)
-                                    .padding(-10)
-                                )
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: rows, 
+                              spacing: spacing) {
+                        ForEach(0..<10, id: \.self) { index in
+                            if index < imagePreviews.count {
+                                Image(uiImage: imagePreviews[index])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50, height: 50)
+                                        .onTapGesture {
+                                            // delete or replace picture
+                                        }
+                            } else {
+                                cameraPlaceholder
+                            }
+                        }
                     }
-            
-            ScrollView(.horizontal) {
-                HStack(spacing: 20) {
-                    ForEach(0..<imagePreviews.count, id: \.self) { index in
-                        Image(uiImage: imagePreviews[index])
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 100, height: 100)
-                            .clipShape(.circle)
-                            
-                    }
+                              
                 }
             }
         }
@@ -64,12 +67,46 @@ struct PhotosPickerView: View {
     PhotosPickerView()
 }
 
+extension PhotosPickerView {
+    
+    private var cameraPlaceholder: some View {
+        Image(systemName: "camera")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(Color.theme.secondaryText.opacity(0.5))
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.theme.secondaryText.opacity(0.5), lineWidth: 1)
+                        .padding(-10)
+                    )
+    }
+}
+
 // MARK: - To Do
 /*
- Placeholder effect, if user does not upload a picture, show the placeholder like Wallapop
+ 
+ - Placeholder effect, if user does not upload a picture, show the placeholder like Wallapop
+ - extract views to extension
+ - update color
+ 
  Image(uiImage: imagePreview ?? UIImage(resource: .airlogo))
      .resizable()
      .aspectRatio(contentMode: .fill)
      .frame(width: 100, height: 100)
+ 
+ code for photospicker array
+ ScrollView(.horizontal) {
+     HStack(spacing: 20) {
+         ForEach(0..<imagePreviews.count, id: \.self) { index in
+             Image(uiImage: imagePreviews[index])
+                 .resizable()
+                 .aspectRatio(contentMode: .fill)
+                 .frame(width: 100, height: 100)
+                 .clipShape(.circle)
+                 
+         }
+     }
+ }
 
  */
