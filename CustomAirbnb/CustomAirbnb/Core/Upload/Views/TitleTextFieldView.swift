1fb31f118795107e4674cfd0901eee392916475b
Oriol Angelet <uriaeri@gmail.com>
2024-02-22 12:32:42 +0100

    TitleTextFieldView extracted to its own file

---
+++ CustomAirbnb/CustomAirbnb/Core/Upload/Views/TitleTextFieldView.swift
@@ -0,0 +1,63 @@
+//
+//  TitleTextFieldView.swift
+//  CustomAirbnb
+//
+//  Created by Uri on 22/2/24.
+//
+
+import SwiftUI
+
+struct TitleTextFieldView: View {
+    
+    @Binding var listingTitle: String
+    @State private var strokeColor: Color = Color.theme.secondaryText.opacity(0.3)
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Listing title")
+                    .foregroundStyle(Color.theme.secondaryText.opacity(0.5))
+                    .padding(.horizontal)
+                    .padding(.top)
+                
+                TextField("",
+                          text: $listingTitle,
+                          onEditingChanged: { editing in
+                    strokeColor = editing ? Color.theme.accent : Color.theme.secondaryText.opacity(0.3)
+                })
+                .autocorrectionDisabled()
+                .foregroundStyle(Color.theme.accent)
+                .padding(.horizontal)
+                .padding(.bottom)
+                .onChange(of: listingTitle) {
+                    if listingTitle.count > 40 {
+                        listingTitle = String(listingTitle.prefix(40))
+                    }
+                }
+            }
+            if !listingTitle.isEmpty {
+                Button(action: {
+                    listingTitle = ""
+                }) {
+                    Image(systemName: "multiply.circle.fill")
+                        .foregroundColor(Color.theme.accent)
+                        .font(.body)
+                        .frame(width: 40)
+                        .padding()
+                }
+            }
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(strokeColor)
+        )
+        .onAppear {
+            strokeColor = Color.theme.secondaryText.opacity(0.3)
+        }
+        .padding(.horizontal, 10)
+    }
+}
+
+#Preview {
+    TitleTextFieldView(listingTitle: .constant("a"))
+}
--- CustomAirbnb/CustomAirbnb/Core/Upload/Views/UploadThirdView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Upload/Views/UploadThirdView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct UploadThirdView: View {
     
     @State private var listingTitle: String = ""
-    @State private var strokeColor: Color = Color.theme.secondaryText.opacity(0.3)
     
     var body: some View {
         NavigationStack {
@@ -21,10 +20,9 @@ struct UploadThirdView: View {
                 VStack(spacing: 20) {
                     progressImage
                     PhotosPickerView()
-                    titleTextField
+                    TitleTextFieldView(listingTitle: $listingTitle)
                     Spacer()
                 }
-                .padding(.horizontal)
             }
             .navigationTitle("List your home")
             .navigationBarTitleDisplayMode(.inline)
@@ -49,49 +47,5 @@ extension UploadThirdView {
             .resizable()
             .scaledToFit()
             .frame(height: 25)
-    }
-    
-    private var titleTextField: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("Listing title")
-                    .foregroundStyle(Color.theme.secondaryText.opacity(0.5))
-                    .padding(.horizontal)
-                    .padding(.top)
-                
-                TextField("",
-                          text: $listingTitle,
-                          onEditingChanged: { editing in
-                    strokeColor = editing ? Color.theme.accent : Color.theme.secondaryText.opacity(0.3)
-                })
-                .autocorrectionDisabled()
-                .foregroundStyle(Color.theme.accent)
-                .padding(.horizontal)
-                .padding(.bottom)
-                .onChange(of: listingTitle) {
-                    if listingTitle.count > 40 {
-                        listingTitle = String(listingTitle.prefix(40))
-                    }
-                }
-            }
-            if !listingTitle.isEmpty {
-                Button(action: {
-                    listingTitle = ""
-                }) {
-                    Image(systemName: "multiply.circle.fill")
-                        .foregroundColor(Color.theme.accent)
-                        .font(.body)
-                        .frame(width: 40)
-                        .padding()
-                }
-            }
-        }
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(strokeColor)
-        )
-        .onAppear {
-            strokeColor = Color.theme.secondaryText.opacity(0.3)
-        }
     }
 }
