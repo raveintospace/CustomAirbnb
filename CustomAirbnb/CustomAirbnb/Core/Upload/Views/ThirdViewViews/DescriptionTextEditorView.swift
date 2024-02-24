7f1e65b9c314953840084b5afb729e1ddb2a5e96
Oriol Angelet <uriaeri@gmail.com>
2024-02-24 13:45:01 +0100

    DescriptionTextEditorView iteration

--- CustomAirbnb/CustomAirbnb/Core/Upload/Views/ThirdViewViews/DescriptionTextEditorView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Upload/Views/ThirdViewViews/DescriptionTextEditorView.swift
@@ -13,42 +13,31 @@ struct DescriptionTextEditorView: View {
     @FocusState private var isTextEditorFocused: Bool
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 1) {
-            HStack {
+        HStack {
+            VStack(alignment: .leading) {
                 Text("Description")
                     .foregroundStyle(Color.theme.secondaryText.opacity(0.5))
                     .padding(.horizontal)
                     .padding(.top)
                 
-                Spacer()
-                
-                if !descriptionText.isEmpty && isTextEditorFocused {
-                    Button(action: {
-                        descriptionText = ""
-                    }) {
-                        Image(systemName: "multiply.circle.fill")
-                            .foregroundColor(Color.theme.accent)
-                            .font(.body)
-                            .frame(width: 20)
-                            .padding(.top)
-                            .padding(.horizontal, 25)
+                TextEditor(text: $descriptionText)
+                    .frame(height: 120)
+                    .autocorrectionDisabled()
+                    .foregroundStyle(Color.theme.accent)
+                    .scrollContentBackground(.hidden)   // avoids a default gray color
+                    .background(Color.theme.background) // avoids a default gray color
+                    .padding(.horizontal, 10)
+                    .focused($isTextEditorFocused)
+                    .onChange(of: descriptionText) {
+                        if descriptionText.count > 3000 {
+                            descriptionText = String(descriptionText.prefix(3000))
+                        }
                     }
-                }
             }
             
-            TextEditor(text: $descriptionText)
-                .frame(height: 120)
-                .autocorrectionDisabled()
-                .foregroundStyle(Color.theme.accent)
-                .scrollContentBackground(.hidden)   // avoids a default gray color
-                .background(Color.theme.background) // avoids a default gray color
-                .padding(.horizontal, 10)
-                .focused($isTextEditorFocused)
-                .onChange(of: descriptionText) {
-                    if descriptionText.count > 3000 {
-                        descriptionText = String(descriptionText.prefix(3000))
-                    }
-                }
+            if !descriptionText.isEmpty && isTextEditorFocused {
+                UploadThirdViewDeletionButton(text: $descriptionText)
+            }
         }
         .overlay(
             RoundedRectangle(cornerRadius: 10)
