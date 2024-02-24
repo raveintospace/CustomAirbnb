1da965a2663058d630abe3f61ef65a73fbad9246
Oriol Angelet <uriaeri@gmail.com>
2024-02-24 14:20:26 +0100

    To Do updated

--- CustomAirbnb/CustomAirbnb/Core/Upload/Views/UploadThirdView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Upload/Views/UploadThirdView.swift
@@ -39,13 +39,13 @@ struct UploadThirdView: View {
             }
             .navigationTitle("List your home")
             .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
             .scrollIndicators(.hidden)
             .toolbar() {
                 ToolbarItem(placement: .topBarLeading) {
                     TopBarLeadingArrow()
                 }
             }
-            .navigationBarBackButtonHidden()
         }
     }
 }
@@ -61,11 +61,63 @@ extension UploadThirdView {
             .resizable()
             .scaledToFit()
             .frame(height: 25)
+            .padding(.top)
     }
 }
 
 /*
  TO DO
+ Background color for navigationbar
+ 
  When Continue button pressed: validate that "integers" are not 0 or negative numbers -> method in VM & user convert string to int // dismiss view and go back to home view with a confirmation message
- keyboard with enter to submit (crypto currency! & SearchBarView)
+ 
+ Keyboard with enter to submit (crypto currency! & SearchBarView)
+ */
+
+/*
+ 
+ Add to this view
+ .navigationBarTitle("List your home")
+ .navigationBarTitleDisplayMode(.inline)
+ .navigationBarBackButtonHidden()
+ .background(
+     NavigationConfigurator { navigationBarAppearance in
+         navigationBarAppearance.configureWithOpaqueBackground()
+         navigationBarAppearance.backgroundColor = .yourDesiredColor
+     }
+ )
+ .scrollIndicators(.hidden)
+ .toolbar {
+     ToolbarItem(placement: .topBarLeading) {
+         TopBarLeadingArrow()
+     }
+ }
+ 
+ Create a new swift file in components
+ struct NavigationConfigurator: View {
+     let configure: (UINavigationBarAppearance) -> Void
+
+     init(configure: @escaping (UINavigationBarAppearance) -> Void) {
+         self.configure = configure
+     }
+
+     var body: some View {
+         VStack {
+             Spacer()
+         }
+         .background(
+             GeometryReader { geometry in
+                 Color.clear.onAppear {
+                     let appearance = UINavigationBarAppearance()
+                     configure(appearance)
+                     UINavigationBar.appearance().standardAppearance = appearance
+                     UINavigationBar.appearance().scrollEdgeAppearance = appearance
+                 }
+                 .frame(height: 0)
+             }
+         )
+     }
+ }
+
+
  */
