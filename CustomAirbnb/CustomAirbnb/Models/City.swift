792ed700eba034259d2eead57c13806467bd7f15
Oriol Angelet <uriaeri@gmail.com>
2024-02-25 12:32:20 +0100

    City Model & ListingDataService updated to convert displayed name of city to the name the api requires

--- CustomAirbnb/CustomAirbnb/Models/City.swift
+++ CustomAirbnb/CustomAirbnb/Models/City.swift
@@ -23,7 +23,7 @@ extension City {
             City(name: "Los Angeles", hoods: ["Arts District", "Beverly Hills", "Chinatown", "Civic Center", "Downtown", "Fairfax", "Hollywood", "Koreatown", "Little Tokio", "Los Feliz", "Playa del Rey", "Venice"]),
             City(name: "Melbourne", hoods: ["Albert Park", "Balaclava", "CBD", "Docklands", "Flemington", "Jolimont", "Kensington", "Port Melbourne", "Princess Hill", "Richmond", "St Kilda", "South Yarra"]),
             City(name: "Paris", hoods: ["Bourse", "Élysée", "Gobelins", "Hôtel-de-Ville", "Louvre", "Luxembourg", "Montmartre", "Observatoire", "Opéra", "Panthéon", "Passy", "Reuilly"]),
-            City(name: "Roma", hoods: ["Aventino", "Campo de Fiori", "EUR", "Esquilino", "Monti", "Pigneto", "Prati", "San Giovani", "Trastevere", "Trevi", "Testaccio", "Vatican City"]),
+            City(name: "Rome", hoods: ["Aventino", "Campo de Fiori", "EUR", "Esquilino", "Monti", "Pigneto", "Prati", "San Giovani", "Trastevere", "Trevi", "Testaccio", "Vatican City"]),
             City(name: "San Diego", hoods: ["Balboa Park", "Bankers Hill", "East Village", "Gaslamp Quarter", "Hillcrest", "La Jolla", "Little Italy", "North Park", "Ocean Beach", "Pacific Beach", "Point Loma", "South Park"]),
             City(name: "Toronto", hoods: ["Annex", "Cabbagetown", "Distillery District", "Harbourfront", "Kensington Market", "Leslieville", "Little Italy", "Queen Street West", "Roncesvalles", "Scarborough", "The Beaches", "Yorkville"])
         ]
