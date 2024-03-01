52e727c4504cbdf77cf2f5ebc2dbabc67b6c95d6
Oriol Angelet <uriaeri@gmail.com>
2024-03-01 18:29:12 +0100

    Updated two cities that were not returning results

--- CustomAirbnb/CustomAirbnb/Models/City.swift
+++ CustomAirbnb/CustomAirbnb/Models/City.swift
@@ -21,11 +21,11 @@ extension City {
             City(name: "Barcelona", hoods: ["Barceloneta", "Ciutat Vella", "Clot", "Eixample", "Fort Pienc", "Gràcia", "Les Corts", "Montbau", "Poblenou", "Poble-sec", "Sagrada Família", "Sants"]),
             City(name: "London", hoods: ["Barnet", "Brent", "Camden", "Chelsea", "City", "Croydon", "Enfield", "Hackney", "Newham", "Southwark", "Tower Hamlets", "Westminster"]),
             City(name: "Los Angeles", hoods: ["Arts District", "Beverly Hills", "Chinatown", "Civic Center", "Downtown", "Fairfax", "Hollywood", "Koreatown", "Little Tokio", "Los Feliz", "Playa del Rey", "Venice"]),
-            City(name: "Melbourne", hoods: ["Albert Park", "Balaclava", "CBD", "Docklands", "Flemington", "Jolimont", "Kensington", "Port Melbourne", "Princess Hill", "Richmond", "St Kilda", "South Yarra"]),
+            City(name: "Madrid", hoods: ["Arganzuela", "Chamberí", "Chueca", "Huertas", "La Latina", "Lavapiés", "Malasaña", "Moncloa", "Retiro", "Salamanca", "Sol", "Tetuán"]),
             City(name: "Paris", hoods: ["Bourse", "Élysée", "Gobelins", "Hôtel-de-Ville", "Louvre", "Luxembourg", "Montmartre", "Observatoire", "Opéra", "Panthéon", "Passy", "Reuilly"]),
             City(name: "Rome", hoods: ["Aventino", "Campo de Fiori", "EUR", "Esquilino", "Monti", "Pigneto", "Prati", "San Giovani", "Trastevere", "Trevi", "Testaccio", "Vatican City"]),
             City(name: "San Diego", hoods: ["Balboa Park", "Bankers Hill", "East Village", "Gaslamp Quarter", "Hillcrest", "La Jolla", "Little Italy", "North Park", "Ocean Beach", "Pacific Beach", "Point Loma", "South Park"]),
-            City(name: "Toronto", hoods: ["Annex", "Cabbagetown", "Distillery District", "Harbourfront", "Kensington Market", "Leslieville", "Little Italy", "Queen Street West", "Roncesvalles", "Scarborough", "The Beaches", "Yorkville"])
+            City(name: "Vancouver", hoods: ["Coal Harbour", "Commercial Drive", "Downtown", "East Vancouver", "Gastown", "Granville Island", "Kerrisdale", "Kitsilano", "Mount Pleasant", "Shaughnessy", "West End", "Yaletown"])
         ]
     }
 }
