1b56737e7868173f03b45ea95c0384c9e5ff6c7a
Oriol Angelet <uriaeri@gmail.com>
2024-03-02 19:38:59 +0100

    LaunchView finished

--- CustomAirbnb/CustomAirbnb/Core/Launch/LaunchView.swift
+++ CustomAirbnb/CustomAirbnb/Core/Launch/LaunchView.swift
@@ -9,36 +9,42 @@ import SwiftUI
 import Vortex
 
 struct LaunchView: View {
+    
+    let cities = City.stub
+    @State private var cityName: String = "Amsterdam"
+    @Binding var showLaunchView: Bool
+    
     var body: some View {
         ZStack {
             Color.launch.background
-                .ignoresSafeArea()
             
             VortexView(.snow) {
-                Text("Amsterdam \nBarcelona \nMadrid")
-                    .multilineTextAlignment(.center)
+                Text(cityName)
                     .foregroundStyle(Color.launch.accent)
                     .font(.largeTitle)
                     .blur(radius: 1)
                     .tag("circle")
+            }
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: 0.35, repeats: true) { timer in
+                        cityName = cities.randomElement()?.name ?? "Amsterdam"
+                }
+            }
+            
+            Image("customlogo")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+                showLaunchView = false
             }
         }
     }
 }
 
 #Preview {
-    LaunchView()
+    LaunchView(showLaunchView: .constant(true))
 }
-
-/*
- ZStack {
- 
- VortexView(.magic) {
-     Text("Barcelona")
-         .foregroundStyle(Color.launch.accent)
-         .font(.largeTitle)
-         .tag("sparkle")
-         .blendMode(.plusLighter)
- }
- }
- */
